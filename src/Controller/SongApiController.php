e32a46091e7a649f1d953c819e9f2c8e6c1fde29
Stjepan Petrovic <spetrovic@4ofthem.eu>
2025-09-26 15:19:46 +0200

    Smart Routes: GET-only & Validate {Wildcards}

--- src/Controller/SongApiController.php
+++ src/Controller/SongApiController.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class SongApiController extends AbstractController
 {
-    #[Route('/api/songs/{id}')]
-    public function getSong($id): Response
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
+    public function getSong(int $id): Response
     {
         $song = [
             'id' => $id,
