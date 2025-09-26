e8216cbf4285af3e44f26a40b8a7b37cf26d7dc5
Stjepan Petrovic <spetrovic@4ofthem.eu>
2025-09-26 11:09:19 +0200

    Wildcard Routes

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use function Symfony\Component\String\u;
 
 final class VinylController
 {
@@ -13,5 +14,17 @@ final class VinylController
     public function homepage(): Response
     {
         return new Response('Title: PB and Jams');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre ' . u(str_replace('-', ' ', $slug))->title();
+        } else {
+            $title = 'All Genres';
+        }
+
+        return new Response($title);
     }
 }
