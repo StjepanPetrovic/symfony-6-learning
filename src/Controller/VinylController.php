ada2835ffd68522485923f3501ec9883b9d277ca
Stjepan Petrovic <spetrovic@4ofthem.eu>
2025-09-26 14:36:23 +0200

    Assets, CSS, Images, etc

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -32,12 +32,10 @@ final class VinylController extends AbstractController
     #[Route('/browse/{slug}')]
     public function browse(string $slug = null): Response
     {
-        if ($slug) {
-            $title = 'Genre ' . u(str_replace('-', ' ', $slug))->title();
-        } else {
-            $title = 'All Genres';
-        }
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
 
-        return new Response($title);
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre,
+        ]);
     }
 }
