b9b18756055de0784c37fbd4f58026b36ad63d8e
Stjepan Petrovic <spetrovic@4ofthem.eu>
2025-09-26 15:12:41 +0200

    JSON API Endpoint

---
+++ src/Controller/SongApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class SongApiController extends AbstractController
+{
+    #[Route('/api/songs/{id}')]
+    public function getSong($id): Response
+    {
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+
+        return $this->json($song);
+    }
+}
